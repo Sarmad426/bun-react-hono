3a11ca235f3ba0778819e6e53d27d04c9cc260ef
Sarmad rafique <sarmadrafique040@gmail.com>
2025-05-01 10:46:15 +0500

    refactor: add console log for todo completion toggle action

--- react-frontend/src/components/TodoItem.tsx
+++ react-frontend/src/components/TodoItem.tsx
@@ -21,9 +21,11 @@ export function TodoItem({
   const [isDeleting, setIsDeleting] = useState(false);
 
   const handleToggleComplete = async () => {
+    console.log("Clicked checkbox for todo:", todo);
     try {
       setIsLoading(true);
       const updatedTodo = await updateTodo(todo.id, {
+        ...todo,
         completed: !todo.completed,
       });
       onTodoUpdated(updatedTodo);
