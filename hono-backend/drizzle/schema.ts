0e11ebbcdb3134c0bec8493ac7724c84845849da
Sarmad rafique <sarmadrafique040@gmail.com>
2025-05-02 11:01:54 +0500

    Removed the user schema from database

--- hono-backend/drizzle/schema.ts
+++ hono-backend/drizzle/schema.ts
@@ -1,5 +1,4 @@
-import { pgTable, index, uuid, varchar, boolean, timestamp, uniqueIndex } from "drizzle-orm/pg-core"
-import { sql } from "drizzle-orm"
+import { pgTable, index, uuid, varchar, boolean, timestamp } from "drizzle-orm/pg-core"
 
 
 
@@ -12,16 +11,4 @@ export const todos = pgTable("todos", {
 	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow().notNull(),
 }, (table) => [
 	index("ix_todo_id").using("btree", table.id.asc().nullsLast().op("uuid_ops")),
-]);
-
-export const userTable = pgTable("userTable", {
-	id: varchar().primaryKey().notNull(),
-	name: varchar().notNull(),
-	email: varchar().notNull(),
-	hashedPassword: varchar("hashed_password"),
-	isGoogleAccount: boolean("is_google_account").notNull(),
-	avatar: varchar(),
-}, (table) => [
-	uniqueIndex("ix_user_email").using("btree", table.email.asc().nullsLast().op("text_ops")),
-	index("ix_user_id").using("btree", table.id.asc().nullsLast().op("text_ops")),
 ]);
